efafd4e0dc771dea1ad2bf9bcf4349cb48e86293
FrancoTardio <a2024110797@estudiantes.upsa.edu.bo>
2024-11-27 12:54:13 -0400

    Create PROYECTOprototipos.h

---
+++ PROYECTOprototipos.h
@@ -0,0 +1,17 @@
+
+#ifndef DBREGISTER_H 
+#define DBREGISTER_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+int OpenTable();
+void addCuidador(const string& nombre, const string& usuario, const string& contrasenha, const string& codigo);
+void addPaciente(const string& nombre, const string& usuario, const string& contrasenha, const string& codigo);
+void handleError(int rc, char* errMsg);
+bool RepeatUserPacientes(const string& RegisterUser);
+bool RepeatUserCuidadores(const string& RegisterUser);
+bool readPacientes(const string& LoginUser, const string& LoginPass);
+bool readCuidadores(const string& LoginUser, const string& LoginPass);
+
+#endif 
